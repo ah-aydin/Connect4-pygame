0ed93e36ea2d016ba645d827704499b186f697b7
ah-aydin <ah-aydin@users.noreply.github.com>
2020-03-16 09:05:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 
 
 import numpy as np
+import pygame
+
+pygame.init()
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+
+# Display variables
+DIST = 100
+WIDTH = COLUMN_COUNT * DIST
+HEIGHT = (ROW_COUNT + 1) * DIST
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def create_board():
     board = np.zeros((ROW_COUNT,COLUMN_COUNT))
@@ -14,9 +24,13 @@ def drop_piece(board, col, player):
     while row > 0 and board[row-1][col] == 0:
         row -= 1
     board[row][col] = player
-    check_win(board, row, col)
+    check_win(board, row, col, player)
 
-def check_win(board, row, col):
+def print_win(board, player):
+    print_board(board)
+    print('Player {} won.'.format(player))
+
+def check_win(board, row, col, player):
     global game_over
 
     # Check the primary diagonal from the indexes (row,col)
@@ -36,11 +50,11 @@ def check_win(board, row, col):
         r += 1
         c += 1
     for i in range(len(lst)-3):
-        truth = [lst[i] != 0, lst[i+1]!=0,lst[i+2]!=0,lst[i+3]!=0]
+        truth = [lst[i]!=0, lst[i+1]!=0, lst[i+2]!=0,lst [i+3]!=0]
         if False not in truth:
             if lst[i] == lst[i+1] and lst[i+1] == lst[i+2] and lst[i+2] == lst[i+3]:
+                print_win(board, player)
                 game_over = True
-                return True
 
     # Check the secondary diagonal from the indexes (row,col)
     lst = []
@@ -59,34 +73,34 @@ def check_win(board, row, col):
         r += 1
         c -= 1
     for i in range(len(lst)-3):
-        truth = [lst[i] != 0, lst[i+1]!=0,lst[i+2]!=0,lst[i+3]!=0]
+        truth = [lst[i]!=0, lst[i+1]!=0, lst[i+2]!=0, lst[i+3]!=0]
         if False not in truth:
             if lst[i] == lst[i+1] and lst[i+1] == lst[i+2] and lst[i+2] == lst[i+3]:
+                print_win(board, player)
                 game_over = True
-                return True
-
 
     # Check the column from the indexes (row, col)
     lst = []
     for i in range(ROW_COUNT):
         lst.append(board[i][col])
     for i in range(len(lst)-3):
-        truth = [lst[i] != 0, lst[i+1]!=0,lst[i+2]!=0,lst[i+3]!=0]
+        truth = [lst[i]!=0, lst[i+1]!=0, lst[i+2]!=0, lst[i+3]!=0]
         if False not in truth:
             if lst[i] == lst[i+1] and lst[i+1] == lst[i+2] and lst[i+2] == lst[i+3]:
+                print_win(board, player)
                 game_over = True
-                return True
 
     # Check the row from the indexes (row, col)
     lst = []
     for i in range(COLUMN_COUNT):
         lst.append(board[row][i])
     for i in range(len(lst)-3):
-        truth = [lst[i] != 0, lst[i+1]!=0,lst[i+2]!=0,lst[i+3]!=0]
+        truth = [lst[i]!=0, lst[i+1]!=0, lst[i+2]!=0, lst[i+3]!=0]
         if False not in truth:
             if lst[i] == lst[i+1] and lst[i+1] == lst[i+2] and lst[i+2] == lst[i+3]:
+                print_win(board, player)
                 game_over = True
-                return True
+
 
 def print_board(board):
     print(np.flip(board, axis=0))
@@ -100,20 +114,30 @@ def is_valid(board, col):
         return False
     return True
 
-board = create_board()
-game_over = False
-turn = 0
+def draw_board(surface, board):
+    pass
 
-while not game_over:
-    print_board(board)
-    while True:
-        col = int(input('Player {} make a selectoin (0-6): '.format(turn+1)))
-        if is_valid(board, col) == True:
-            break
-    drop_piece(board, col, turn+1)
-    turn = (turn + 1) % 2
+def main():
+    global board
+    global game_over
 
-print_board(board)
-if turn == 0:
-    turn = 2
-print('Player {} won.'.format(turn))
+    board = create_board()
+    game_over = False
+    turn = 0
+
+    while not game_over:
+        print_board(board)
+        while True:
+            col = int(input('Player {} make a selectoin (0-6): '.format(turn+1)))
+            if is_valid(board, col) == True:
+                break
+        drop_piece(board, col, turn+1)
+        turn = (turn + 1) % 2
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+if __name__ == '__main__':
+    main()
